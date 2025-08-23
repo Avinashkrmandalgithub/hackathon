70061d8ec1043dfd56d8ea522f69107f49396350
Avinash <avinashkrmandal03@gmail.com>
2025-08-23 11:19:57 +0530

    register and login testing done, required changes for the middlewares !?

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getUserProfile, loginUser, registerUser } from '../controllers/user.controller.js';
-import authMiddleware from '../middlewares/authMiddleware.js';
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+
 
 const router = express.Router();
 
